2ee182578e63a75f15cbce826cd268f91e0df13e
Yurii Rybachok <drummer2009@ukr.net>
2023-08-08 17:44:38 +0300

    create AddTicker & AddButton components

---
+++ src/components/AddTicker.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700"
+          >Тикер</label
+        >
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+            v-model="ticker"
+            @keydown.enter="add"
+            type="text"
+            name="wallet"
+            id="wallet"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE"
+          />
+        </div>
+        <!-- <div
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+            >
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                BTC
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                DOGE
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                BCH
+              </span>
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                CHD
+              </span>
+            </div>
+            <div class="text-sm text-red-600">Такой тикер уже добавлен</div> -->
+      </div>
+    </div>
+    <add-button @click="add" type="button" class="my-4" />
+  </section>
+</template>
+
+<script>
+import AddButton from "./AddButton.vue";
+
+export default {
+  name: "AddTicker",
+  components: {
+    AddButton,
+  },
+  data() {
+    return { ticker: "" };
+  },
+  methods: {
+    add() {
+      this.$emit("add-ticker", this.ticker);
+      this.ticker = "";
+    },
+  },
+};
+</script>
